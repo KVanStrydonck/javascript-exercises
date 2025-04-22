6c71abe3402a6b25ea90c9c37250ac922cba907c
Kristoffer <krisvanstr@icloud.com>
2025-04-22 21:19:20 +0200

    add solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (inputString, repeatAmount) {
+  if (repeatAmount < 0) {
+    return "ERROR";
+  }
 
+  let result = "";
+
+  for (let i = 0; i < repeatAmount; i++) {
+    result += inputString;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
