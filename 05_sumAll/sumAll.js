e01822fcc052a56fc59769145a4e424aed90d7c0
Kristoffer <krisvanstr@icloud.com>
2025-04-23 16:26:16 +0200

    add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (begin, end) {
+  if (begin < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(begin) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+
+  if (isNaN(begin) || isNaN(end)) {
+    return "ERROR";
+  }
+
+  if (begin > end) {
+    let temp = begin;
+    begin = end;
+    end = temp;
+  }
+
+  let sum = 0;
+
+  for (let i = begin; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
