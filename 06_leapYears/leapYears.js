9997a50e427bf7c2b477ac0b3917a21e0f355291
Kristoffer <krisvanstr@icloud.com>
2025-04-24 08:58:37 +0200

    add solution to 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let divisibleByHundred = year % 100 === 0;
+  let divisibleByFourHundred = year % 400 === 0;
+  let divisibleByFour = year % 4 === 0;
 
+  if (!divisibleByFour) {
+    return false;
+  }
+
+  if (divisibleByHundred && !divisibleByFourHundred) {
+    return false;
+  }
+
+  return true;
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
