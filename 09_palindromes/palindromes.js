6f58d1889b30ac9f1913722696bec611caff6dad
Kristoffer <krisvanstr@icloud.com>
2025-04-29 10:22:42 +0200

    add solution 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  for (let i = 0; i < Math.floor(cleanStr.length / 2); i++) {
+    const charFromStart = cleanStr[i];
+    const charFromEnd = cleanStr[cleanStr.length - 1 - i];
+    console.log(`Comparing ${charFromStart} with ${charFromEnd}`);
+    if (charFromStart !== charFromEnd) {
+      console.log(`Found a mismatch: ${charFromStart} and ${charFromEnd}`);
+      return false;
+    }
+  }
+  console.log(`${cleanStr} is a palindrome!`);
+  return true;
 };
+
+palindromes("hello");
 
 // Do not edit below this line
 module.exports = palindromes;
